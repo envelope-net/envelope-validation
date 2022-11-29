3f4c30cc77ab534090e1041f3fe79d3371636aa8
Tomas Pilat <tomas.pilat@disig.sk>
2022-11-29 23:58:19 +0100

    added EmailValidator

--- src/Envelope.Validation/Validators/PropertyValidators/EmailValidator.cs
+++ src/Envelope.Validation/Validators/PropertyValidators/EmailValidator.cs
@@ -1,23 +1,14 @@
-﻿using Envelope.Validation.Client;
+﻿using Envelope.Extensions;
+using Envelope.Reflection.ObjectPaths;
+using Envelope.Validation.Client;
+using Envelope.Validation.Helpers;
 using Envelope.Validation.Internal;
 using Envelope.Validation.Results;
-using Envelope.Extensions;
-using Envelope.Reflection.ObjectPaths;
-using System.Text.RegularExpressions;
 
 namespace Envelope.Validation.Validators.PropertyValidators;
 
 internal class EmailValidator<T> : PropertyValidator<T, string?>
 {
-	private const string _expression = @"^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-||_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+([a-z]+|\d|-|\.{0,1}|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])?([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$";
-	private static readonly Lazy<Regex> _regex = new(() => CreateRegEx());
-
-	private static Regex CreateRegEx()
-	{
-		const RegexOptions options = RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.ExplicitCapture;
-		return new Regex(_expression, options, TimeSpan.FromSeconds(2.0));
-	}
-
 	private const string DEFAULT_ValidationMessage = "Is not a valid email address.";
 	private const string DEFAULT_ValidationMessageWithProperty = "'{PropertyName}' is not a valid email address.";
 
@@ -50,7 +41,7 @@ internal class EmailValidator<T> : PropertyValidator<T, string?>
 		if (context is not ValidationContext<T, string?> ctx)
 			throw new ArgumentException($"{nameof(context)} must be type of {typeof(ValidationContext<T>).FullName}", nameof(context));
 
-		if (ctx.ValueToValidate == null || _regex.Value.IsMatch(ctx.ValueToValidate))
+		if (ctx.ValueToValidate == null || EmailValidator.IsValidEmail(ctx.ValueToValidate))
 			return null;
 		else
 			return new ValidationResult(
